0dba8c4c203ea19c91dfc3a3ed0cd3ff7ae52385
unknown <gershom.seneviratne@gmail.com>
2023-08-31 05:04:14 -0400

    Labs for Fall 2023

---
+++ Motors_Encoders/src/main.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+#include <Encoder.h>
+
+const unsigned int M1_ENC_A = 39;
+const unsigned int M1_ENC_B = 38;
+const unsigned int M2_ENC_A = 37;
+const unsigned int M2_ENC_B = 36;
+
+const unsigned int M1_IN_1;
+const unsigned int M1_IN_2;
+const unsigned int M2_IN_1;
+const unsigned int M2_IN_2;
+
+const unsigned int M1_IN_1_CHANNEL = 8;
+const unsigned int M1_IN_2_CHANNEL = 9;
+const unsigned int M2_IN_1_CHANNEL = 10;
+const unsigned int M2_IN_2_CHANNEL = 11;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+const unsigned int PWM_VALUE = 512; // Max PWM given 8 bit resolution
+
+const int freq = 5000;
+const int resolution = 10;
+
+void setup() {
+  // Stop the right motor by setting pin 14 low
+  // this pin floats high or is pulled
+  // high during the bootloader phase for some reason
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+  delay(100);
+
+  Serial.begin(115200);
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
+}
+
+void loop() {
+  // Create the encoder objects after the motor has
+  // stopped, else some sort exception is triggered
+  Encoder enc1(M1_ENC_A, M1_ENC_B);
+  Encoder enc2(M2_ENC_A, M2_ENC_B);
+
+  while(true) {
+    long enc1_value = enc1.read();
+    long enc2_value = enc2.read();
+  }
+}
