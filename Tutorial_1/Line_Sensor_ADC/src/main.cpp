6e07bc8f6dc2e01a57b8ecf7c64b4f62ded2b395
Elijah Martin <e.troymartin@gmail.com>
2023-08-31 11:38:01 -0400

    Detect light and dark surfaces

--- Tutorial_1/Line_Sensor_ADC/src/main.cpp
+++ Tutorial_1/Line_Sensor_ADC/src/main.cpp
@@ -10,17 +10,27 @@ const unsigned int ADC_2_CS = 17;
 int adc1_buf[8];
 int adc2_buf[8];
 
+int threshold = 700; // threshold value for light and dark
+
 void readADC() {
   for (int i = 0; i < 8; i++) {
     adc1_buf[i] = adc1.readADC(i);
     adc2_buf[i] = adc2.readADC(i);
 
     if (i<7) {
-      Serial.print(adc1_buf[i]); Serial.print("\t");
+      if (adc1_buf[i] > threshold) {
+        Serial.print(0); Serial.print("\t"); // floor
+      } else {
+        Serial.print(1); Serial.print("\t"); // white tape
+      }
     }
 
     if (i<6) {
-      Serial.print(adc2_buf[i]); Serial.print("\t");
+      if (adc2_buf[i] > threshold) {
+        Serial.print(0); Serial.print("\t"); // floor
+      } else {
+        Serial.print(1); Serial.print("\t"); // white tape
+      }
     }
   }
 }
