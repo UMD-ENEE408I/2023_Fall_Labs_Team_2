c02637da3657fe428eb4592d0ad5d7f7c6e72ac1
Elijah Martin <43558190+Elij4hMartin@users.noreply.github.com>
2023-12-17 00:25:24 -0500

    Updated ball tracker to communicate to robot
    Modified main function

--- RobotPetPy/ball_tracker.py
+++ RobotPetPy/ball_tracker.py
@@ -3,7 +3,15 @@ import math
 import numpy as np
 from ultralytics import YOLO
 from pupil_apriltags import Detector
+import socket
 
+# Network variables
+robotIP = "192.168.137.5"
+port = 4210
+ 
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+robot_address = (robotIP, port)
 
 class vision:
     def __init__(self, vid_device):
@@ -36,8 +44,8 @@ class vision:
         widthB = round(center[0])
         # Drawing the lines'
         # print(heightB)
-        cv2.line(img, (0, heightB), (width, heightB), (0, 0, 255), 2)
-        cv2.line(img, (widthB, 0), (widthB, height), (0, 0, 255), 2)
+        cv2.line(img, (0, heightB), (width, heightB), (0, 0, 0), 2)
+        cv2.line(img, (widthB, 0), (widthB, height), (0, 0, 0), 2)
         return img
 
     def getTag(self):
@@ -78,33 +86,38 @@ class vision:
         obj_dict = {}
         for result in results:
             detection_count = result.boxes.shape[0]
-            print("Number of objects in frame: {}".format(detection_count))
+            ##print("Number of objects in frame: {}".format(detection_count))
 
             for i in range(detection_count):
                 cls = int(result.boxes.cls[i].item())
                 name = result.names[cls]
                 confidence = float(result.boxes.conf[i].item())
-                if name in detect and confidence > 0.85:
+                if name in detect and confidence > 0.5:
                     bounding_box = result.boxes.xyxy[i].cpu().numpy()
                     x = int(bounding_box[0])
                     y = int(bounding_box[1])
                     width = int(bounding_box[2] - x)
                     height = int(bounding_box[3] - y)
+                    # Diagonal size XY axis
+                    sizeXY = int(math.sqrt(width*height))
+                    #Center x and y 
+                    x = int(x + width/2)
+                    y = int(y + height/2)
                     if name not in obj_dict.keys():
-                        obj_dict[name] = [x,y,width,height]
-                    if (obj_dict[name][3] < height):
-                        obj_dict[name] = [x,y,width,height]
+                        obj_dict[name] = [x,y,sizeXY]
+                    if (obj_dict[name][2] < sizeXY):
+                        obj_dict[name] = [x,y,sizeXY]
 
             if(len(obj_dict.keys()) > 0):
                 for keys in obj_dict.keys():
-                    new_frame = self.drawCross(new_frame,[obj_dict[keys][2],obj_dict[keys][3]])
+                    new_frame = self.drawCross(new_frame,[obj_dict[keys][0],obj_dict[keys][1]])
         # return new_frame #for debug
         return new_frame,obj_dict #rea; return
 
     def __del__(self):
         self.vid.release()
         cv2.destroyAllWindows() 
-        print('Destructor called, vision deleted')
+        ##print('Destructor called, vision deleted')
 
     def colorTrack(self):
         img = self.frame
@@ -136,7 +149,7 @@ class vision:
 
         contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) > 1:
-            print(len(contours[0])) 
+            ##print(len(contours[0])) 
             maxContour = contours[0]
             for obj in  contours:
                 if len(obj) > len(maxContour):
@@ -146,11 +159,11 @@ class vision:
 
         # print(len(contours))
         if  len(contours) == 0:
-            print("no object detected")
+            ##print("no object detected")
             center = None
         else:
             center = self.findcenter(contours)
-            print("center:{}".format(center))
+            ##print("center:{}".format(center))
 
 
         #     # print(len(contours[0]))
@@ -186,15 +199,41 @@ class vision:
         center.append((leftTy[0][1]+Bottomy[0][1])/2)
         return center
 
+#convert value to arduino BYTE
+def numToByteRange(num, inMin, inMax):
+  return int((num - inMin) / (inMax - inMin) * (255-1))
+    
 
 if __name__ == '__main__':
     cam = vision(0)
+    arduinoX = 0
+    arduinoY = 0
+    arduinoSizeXY = 0
 
     while True:
         cam.getFrame()
         # output,center = cam.colorTrack()
-        output,objects = cam.objectDetection(["person","cat"])
-        print(objects.keys())
+        output,objects = cam.objectDetection(["person","banana"])
+        # check if tag is in photo
+        if "banana" in objects.keys():
+            print("Banana: X:" + str(objects["banana"][0]) + " Y:" + str(objects["banana"][1]) + " SizeXY:" + str(objects["banana"][2]))
+        if "person" in objects.keys():
+            print("Person: X:" + str(objects["person"][0]) + " Y:" + str(objects["person"][1]) + " SizeXY:" + str(objects["person"][2]))
+            # map outputs to arduino
+            # map size from X size of screen
+            arduinoX = numToByteRange(objects["person"][0], 1, output.shape[0])
+            # map size from Y size of screen
+            arduinoY = numToByteRange(objects["person"][1], 1, output.shape[1])
+            # map size from XY size of screen
+            arduinoSizeXY = numToByteRange(objects["person"][2], 1, math.sqrt(output.shape[0]*output.shape[1]))
+        else:
+            arduinoX = 0
+            arduinoY = 0
+            arduinoSizeXY = 0
+        print("Camera XY:" + str(output.shape[0]) + " " + str(output.shape[1]))
+        print("Data: " + str(arduinoX) + " " + str(arduinoY) + " " + str(arduinoSizeXY))
+        # Send data to robot
+        sock.sendto(("H" + chr(arduinoX) + chr(arduinoSizeXY)).encode('utf-8'), robot_address)
         # output = cam.drawCross(output)
         cv2.imshow("img", output)
         if cv2.waitKey(1) & 0xFF == ord('q'): 
